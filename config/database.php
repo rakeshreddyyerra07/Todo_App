9c96e4156f5c49788d43b2ea86e67900fa16eeb3
Rakesh5834 <rakeshyerra2001@gmail.com>
2026-09-02 16:30:46 +0530

    Update database connection for Wasmer

--- config/database.php
+++ config/database.php
@@ -1,23 +1,23 @@
 <?php
 
-$severname = "localhost";
-$username= "root";
-$password= "";
-$dbname= "todo_app";
+$servername = getenv("db.fr-roub1.bengt.wasmernet.com");
+$username   = getenv("user_6c0af212");
+$password   = getenv("Dpw_LtxQbMJ30UpOdtcFibygjflYPE8wVzuC");
+$dbname     = getenv("db_100af6ff);
+$port       = getenv("20184);
 
 $conn = mysqli_connect(
-    $severname,
+    $servername,
     $username,
     $password,
-    $dbname
+    $dbname,
+    (int)$port
 );
 
 if (!$conn) {
-
-die(
-     "Database connection failed: "
-        . mysqli_connect_error()
-);
+    die("Database connection failed: " . mysqli_connect_error());
 }
-?>
 
+$conn->set_charset("utf8mb4");
+
+?>
--- sql/todo_app (2).sql
+++ sql/todo_app (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 31, 2026 at 03:16 PM
+-- Generation Time: Sep 01, 2026 at 10:57 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -102,7 +102,8 @@ INSERT INTO `login_logs` (`id`, `emailaddress`, `ipaddress`, `latlang`, `locatio
 (57, 'rakeshreddy5834@gmail.com', '49.43.225.63', '17.476848180548586, 78.36323873375076', 'Hyderabad, Telangana, India', '2026-08-31 13:00:22', 'FAILED', '2026-08-31 16:30:22'),
 (58, 'rakeshreddy5834@gmail.com', '49.43.225.63', '17.476848180548586, 78.36323873375076', 'Hyderabad, Telangana, India', '2026-08-31 13:00:24', 'FAILED', '2026-08-31 16:30:24'),
 (59, 'rakeshreddy5834@gmail.com', '49.43.225.63', '17.476848180548586, 78.36323873375076', 'Hyderabad, Telangana, India', '2026-08-31 13:00:25', 'FAILED', '2026-08-31 16:30:25'),
-(60, 'rakeshreddy5834@gmail.com', '49.43.225.63', '17.476848180548586, 78.36323873375076', 'Hyderabad, Telangana, India', '2026-08-31 13:10:29', 'SUCCESS', '2026-08-31 16:40:29');
+(60, 'rakeshreddy5834@gmail.com', '49.43.225.63', '17.476848180548586, 78.36323873375076', 'Hyderabad, Telangana, India', '2026-08-31 13:10:29', 'SUCCESS', '2026-08-31 16:40:29'),
+(61, 'rakeshreddy5834@gmail.com', '49.43.225.13', '17.476761147020422, 78.36323678481901', 'Hyderabad, Telangana, India', '2026-09-01 10:12:26', 'SUCCESS', '2026-09-01 13:42:26');
 
 -- --------------------------------------------------------
 
@@ -166,30 +167,26 @@ INSERT INTO `tasks` (`id`, `Task`, `Description`, `status`, `is_completed`, `pri
 (20, 'Testing Site', NULL, 0, 0, 'Medium', 'Todo', '2026-08-25 15:40:46', '2026-08-26 11:31:11'),
 (21, 'test 1', NULL, 0, 0, 'Medium', 'Todo', '2026-08-26 09:07:02', '2026-08-26 11:31:05'),
 (23, 'Task manager', NULL, 2, 0, 'Medium', 'Todo', '2026-08-26 11:59:18', '2026-08-26 12:08:45'),
-(25, 'Test App', NULL, 2, 0, 'Medium', 'Todo', '2026-08-26 12:58:17', '2026-08-26 12:58:20'),
+(25, 'Test App', NULL, 1, 0, 'Medium', 'Todo', '2026-08-26 12:58:17', '2026-08-31 13:55:25'),
 (26, 'test', NULL, 2, 0, 'Medium', 'Todo', '2026-08-26 13:59:01', '2026-08-26 13:59:12'),
 (27, 'data tests', NULL, 2, 0, 'Medium', 'Todo', '2026-08-27 07:30:02', '2026-08-27 07:30:20'),
 (28, 'data test', NULL, 2, 0, 'Medium', 'Todo', '2026-08-27 07:31:44', '2026-08-27 07:32:04'),
 (29, 'glass door', NULL, 2, 0, 'Medium', 'Todo', '2026-08-27 07:37:37', '2026-08-27 07:40:10'),
 (30, 'apps', NULL, 2, 1, 'Medium', 'Todo', '2026-08-27 07:38:07', '2026-08-31 08:55:13'),
 (31, 'Prepare a Glass door', 'Check measurements and prepare materials.', 1, 1, 'Medium', 'Done', '2026-08-27 08:02:23', '2026-08-31 10:09:14'),
-(33, 'test data', 'check the data', 2, 1, 'Medium', 'Todo', '2026-08-27 08:11:12', '2026-08-31 09:00:47'),
-(34, 'data prepare', 'check the data', 2, 0, 'Medium', 'Todo', '2026-08-27 08:17:04', '2026-08-27 08:17:28'),
-(35, 'test data', 'data', 2, 0, 'Medium', 'Todo', '2026-08-27 08:18:59', '2026-08-27 08:20:08'),
-(36, 'test data', 'hgcfh', 2, 0, 'Medium', 'Todo', '2026-08-27 08:20:57', '2026-08-27 08:22:47'),
-(38, 'test data', 'hfgh', 0, 1, 'Medium', 'Todo', '2026-08-27 08:43:06', '2026-08-31 09:01:01'),
+(33, 'test data', 'check the data', 1, 1, 'Medium', 'Todo', '2026-08-27 08:11:12', '2026-08-31 13:56:05'),
+(34, 'data prepare', 'check the data', 1, 0, 'Medium', 'Todo', '2026-08-27 08:17:04', '2026-08-31 13:55:38'),
+(35, 'test data', 'data', 1, 0, 'Medium', 'Todo', '2026-08-27 08:18:59', '2026-08-31 13:55:31'),
 (42, 'Prepare a Glass door', 'Prepare glass door for installation', 1, 1, 'Medium', 'Done', '2026-08-27 09:09:20', '2026-08-31 10:00:49'),
 (43, 'test data', 'app', 1, 0, 'Medium', 'Review', '2026-08-27 09:09:31', '2026-08-31 08:56:11'),
-(44, 'test data', 'n m', 0, 1, 'Medium', 'Todo', '2026-08-27 13:45:05', '2026-08-31 09:09:06'),
 (45, 'Prepare a Glass door', 'Prepare Glass door and installed it', 1, 0, 'Medium', 'In Progress', '2026-08-27 13:46:56', '2026-08-31 10:08:59'),
-(46, 'Prepare a Glass door', 'wcbkjna', 2, 1, 'Medium', 'Todo', '2026-08-28 16:37:45', '2026-08-31 09:00:54'),
 (47, 'test data', 'test data', 1, 1, 'Medium', 'In Progress', '2026-08-28 17:30:44', '2026-08-31 10:30:51'),
-(48, 'test', NULL, 1, 0, 'Medium', 'Todo', '2026-08-31 07:17:20', '2026-08-31 10:49:25'),
-(49, 'Test Glass', NULL, 1, 1, 'Low', 'Done', '2026-08-31 07:19:48', '2026-08-31 10:33:10'),
-(50, 'glass door', NULL, 1, 0, 'Medium', 'In Progress', '2026-08-31 08:05:35', '2026-08-31 08:39:58'),
+(50, 'Size\r\nVerify measurements', NULL, 1, 1, 'Medium', 'In Progress', '2026-08-31 08:05:35', '2026-08-31 13:52:14'),
 (52, 'Prepare a  door', 'test the door and installed it', 1, 1, 'Medium', 'Done', '2026-08-31 09:10:09', '2026-08-31 10:21:20'),
 (55, 'test data', 'data', 1, 1, 'Low', 'Done', '2026-08-31 09:38:40', '2026-08-31 09:42:24'),
-(56, 'Prepare a Glass door', 'prepare based on requriement & installed it', 1, 1, 'Low', 'Done', '2026-08-31 09:40:44', '2026-08-31 09:41:47');
+(56, 'Prepare a Glass door', 'prepare based on requriement & installed it', 1, 1, 'Low', 'Done', '2026-08-31 09:40:44', '2026-08-31 09:41:47'),
+(57, 'Install Main Door', 'Install main entrance', 1, 1, 'Low', 'Done', '2026-08-31 13:49:14', '2026-08-31 13:49:59'),
+(58, 'Check Door Size', 'Size\r\nVerify measurements', 1, 1, 'Medium', 'Todo', '2026-08-31 13:50:58', '2026-09-01 08:14:55');
 
 -- --------------------------------------------------------
 
@@ -255,7 +252,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `login_logs`
 --
 ALTER TABLE `login_logs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
 
 --
 -- AUTO_INCREMENT for table `password_resets`
@@ -267,7 +264,7 @@ ALTER TABLE `password_resets`
 -- AUTO_INCREMENT for table `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
 
 --
 -- AUTO_INCREMENT for table `users`
