869d9a31029f4450fe33d78985307991df297690
Rakesh5834 <rakeshyerra2001@gmail.com>
2026-09-03 17:46:27 +0530

    Update Todo App

--- config/database.php
+++ config/database.php
@@ -1,10 +1,10 @@
 <?php
 
-$servername = "localhost";
-$username   = "root";
-$password   = "";
-$dbname     = "todo_app";
-$port       = 3306;
+$servername = "db.fr-roub1.bengt.wasmernet.com";
+$username   = "user_6c0af212";
+$password   = "pw_LtxQbMJ30UpOdtcFibygjflYPE8wVzuC";
+$dbname     = "db_100af6ff";
+$port       = 20184;
 
 $conn = mysqli_connect(
     $servername,
@@ -19,3 +19,4 @@ if (!$conn) {
 }
 
 $conn->set_charset("utf8mb4");
+
